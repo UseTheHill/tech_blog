62e7ea4762f79ea45849415130dbd3d651e5e7e1
Kayte McDonough <kaytemcdonough@gmail.com>
2021-05-13 12:13:49 -0400

    updated seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,22 @@
-const sequelize = require("../config/connection");
-const { User } = require("../models");
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
 
-const userData = require("./userData.json");
-const postData = require("./postData.json");
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, postData, {
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  const post = await Post.bulkCreate(postData, {
+    individualHooks: true,
+    returning: true,
+  });
+  const comments = await Comment.bulkCreate(commentData, {
     individualHooks: true,
     returning: true,
   });
