9aa210fd6064314a627a9f595b1d07faa108cbb7
Kayte McDonough <kaytemcdonough@gmail.com>
2021-05-13 12:16:10 -0400

    added hasMany, belongsTo in index.js

--- models/index.js
+++ models/index.js
@@ -1,13 +1,43 @@
-const User = require("./User");
+// import models
 const Post = require("./Post");
+const Comment = require("./Comment");
+const User = require("./User");
 
+// Post belongs to User
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+// User have many posts
 User.hasMany(Post, {
   foreignKey: "user_id",
   onDelete: "CASCADE",
 });
 
-Post.belongsTo(User, {
+// Comment belongs to User
+Comment.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-module.exports = { User, Post };
+// User have many comments
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+// Comment belongs to Post
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+// Post have many comments
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+module.exports = {
+  Post,
+  Comment,
+  User,
+};
